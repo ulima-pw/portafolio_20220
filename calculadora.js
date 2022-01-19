e53646950cb48b82d8d0cd3b211ddc3a00a695b9
Hernan <hernan@jsatch.com>
2022-01-19 15:57:15 -0500

    Calculadora con js no obstrusivo

--- calculadora.js
+++ calculadora.js
@@ -1,7 +1,10 @@
 let numeroIngresadoStr = 0 // global
 let enOperacion = false;
 
-function digitoOnClick(numero) {
+function digitoOnClick(event) {
+    const but = event.target
+    const numero = but.innerText
+
     const input = document.getElementById("txt_numeros")
     
     if (input.value == "0" || enOperacion) {
@@ -13,7 +16,10 @@ function digitoOnClick(numero) {
     }
 }
 
-function operacionOnClick(tipoOperacion) {
+const operacionOnClick = (event) => {
+    const but = event.target
+    const tipoOperacion = but.innerText;
+
     const input = document.getElementById("txt_numeros")
     if (tipoOperacion == "C") {
         input.value = "0"
@@ -25,3 +31,23 @@ function operacionOnClick(tipoOperacion) {
         input.value = resultado.toString()
     }
 }
+
+const main = function() {
+    // Registrar los eventos click de los botones
+    document.getElementById("but0").onclick = digitoOnClick
+    document.getElementById("but1").onclick = digitoOnClick
+    document.getElementById("but2").onclick = digitoOnClick
+    document.getElementById("but3").onclick = digitoOnClick
+    document.getElementById("but4").onclick = digitoOnClick
+    document.getElementById("but5").onclick = digitoOnClick
+    document.getElementById("but6").onclick = digitoOnClick
+    document.getElementById("but7").onclick = digitoOnClick
+    document.getElementById("but8").onclick = digitoOnClick
+    document.getElementById("but9").onclick = digitoOnClick
+
+    document.getElementById("butC").onclick = operacionOnClick
+    document.getElementById("butIgual").onclick = operacionOnClick
+    document.getElementById("butMas").onclick = operacionOnClick
+}
+
+main()
