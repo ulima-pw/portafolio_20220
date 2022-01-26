6b5930db1e6c8b0a1c92263c740a3bfcc37641a3
Hernan <hernan@jsatch.com>
2022-01-26 17:00:57 -0500

    Segunda llamada AJAX implementada

--- index.js
+++ index.js
@@ -12,11 +12,48 @@ const crearDivImagenCarrusel = (urlImagen, esPrimera) => {
 
 }
 
-const callbackSuccess = (response) => {
-    return response.json()
+// const callbackSuccess = (response) => {
+//     return response.json()
+// }
+
+// const callbackReadResponse = (data) => {
+    
+// }
+
+const callbackError = (error) => {
+    console.error(error)
 }
 
-const callbackReadResponse = (data) => {
+const crearTrProyectos = (proyecto) => {
+    /*
+    <tr>
+      <td>proyecto.nombre</td>
+      <td>proyecto.usuario</td>
+      <td>proyecto.rating</td>
+    </tr>
+    */
+    const tr = document.createElement("tr")
+
+    const tdNombre = document.createElement("td")
+    const tdUsuario = document.createElement("td")
+    const tdRating = document.createElement("td")
+
+    tdNombre.innerText = proyecto.nombre
+    tdUsuario.innerText = proyecto.usuario
+    tdRating.innerText = proyecto.rating
+
+    tr.appendChild(tdNombre)
+    tr.appendChild(tdUsuario)
+    tr.appendChild(tdRating)
+
+    return tr
+}
+
+const cargarDatosAsyncAwait = async () => {
+    const response = await fetch("http://demo9667197.mockable.io/") // hago la llamada
+    const data = await response.json() // lo vuelvo objeto javascript
+
+    console.log("Termino carga de imagenes")
     const arrImagenes = data.images
     let esPrimera = true;
     for (let url of arrImagenes) {
@@ -24,19 +61,55 @@ const callbackReadResponse = (data) => {
         document.getElementById("carrusel").appendChild(div)
         esPrimera = false
     }
+
+    const responseProyectos = await fetch("http://demo9667197.mockable.io/proyectos")
+    const dataProyectos = await responseProyectos.json()
+
+    console.log("Termino carga de proyectos")
+    const proyectos = dataProyectos.proyectos
+
+    for (let proy of proyectos) {
+        const tr = crearTrProyectos(proy)
+        document.getElementById("data_proyectos").appendChild(tr)
+    }
 }
 
-const callbackError = (error) => {
-    console.error(error)
+const cargarDatos = () => {
+    fetch("http://demo9667197.mockable.io/")
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log("Termino carga de imagenes")
+            const arrImagenes = data.images
+            let esPrimera = true;
+            for (let url of arrImagenes) {
+                const div = crearDivImagenCarrusel(url, esPrimera)
+                document.getElementById("carrusel").appendChild(div)
+                esPrimera = false
+            }
+
+            fetch("http://demo9667197.mockable.io/proyectos")
+                .then((response) => {
+                    return response.json()
+                }).then((data) => {
+                    console.log("Termino carga de proyectos")
+                    console.log(data)
+                }).catch(callbackError)
+        })
+        .catch(callbackError)
 }
 
+
+// const cargarProyectos = () => {
+    
+// }
 
 
 const main = () => {
-    fetch("http://demo9667197.mockable.io/")
-        .then(callbackSuccess)
-        .then(callbackReadResponse)
-        .catch(callbackError)
+    // Cargar primero las imagenes y luego proyectos
+    cargarDatosAsyncAwait()
+    //cargarProyectos()
 }
 
 window.addEventListener("load", main)
